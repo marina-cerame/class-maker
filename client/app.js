fdd1d3e17cfc510afaf1d4eb1d1733067d3b8ba1
Marina Cerame <marina.cerame@gmail.com>
2016-12-19 16:39:05 -0600

    deployment

--- client/app.js
+++
@@ -1,8 +0,0 @@
-var classMaker = angular.module('classMaker', ['ngRoute']);
-
-classMaker.config(function($routeProvider, $httpProvider) {
-  $routeProvider
-    .when('/login', {
-      templateUrl: '/login/login.html',
-    });
-});

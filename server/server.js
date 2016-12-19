168e914c23dcaf1e7864b010c23203167d48306b
Marina Cerame <marina.cerame@gmail.com>
2016-12-19 13:10:51 -0600

    helps if you spell the path name right

--- server/server.js
+++ server/server.js
@@ -17,5 +17,5 @@ app.listen(8080);
 console.log('App listening on port 8080');
 
 app.get('/', function(request, response) {
-  response.sendfile('./public/index.html');
+  response.sendfile('./client/index.html');
 });
